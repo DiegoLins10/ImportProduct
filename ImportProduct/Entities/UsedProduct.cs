dfde249b611e21b7ff829535894d67fb1304c2e9
diegolins10 <diegofernandeslins@gmail.com>
2021-07-16 19:51:24 -0300

    sub class

---
+++ ImportProduct/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ImportProduct.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string priceTag()
+        {
+            return Name + "(used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture)
+            +" (Manufacture date: " + ManufactureDate.ToString("dd/MM/yyyy") + ")";
+        }
+    }
+}
